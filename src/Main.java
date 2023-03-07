2969f6a30fb0de6dfbabd153606f521213eb54a4
Linke <linke@duck.com>
2023-03-08 02:50:39 +0300

    task 3 complete task 2 fix

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1() {
         int salary = 15000;
         int total = 0;
-        int month = 1;
+        int month = 0;
         while (total < 2_459_000) {
             total = total + salary;
             month++;
@@ -22,6 +23,18 @@ public class Main {
         System.out.println();
         for(i = 10; i > 0; i--) {
             System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    public static void task3() {
+        int people = 12_000_000;
+        int death = 8 * 12000;
+        int life = 17 * 12000;
+        int year = 0;
+        while (year < 10) {
+            year++;
+            people = people + (life - death);
+            System.out.println("Год " + year + " численность населения составляет " + people);
         }
     }
 }
