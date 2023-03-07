e07ec8561e5842e5b5074cd4b028cadfe3738d3c
Linke <linke@duck.com>
2023-03-08 01:59:28 +0300

    task 1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        int salary = 15000;
+        int total = 0;
+        int month = 1;
+        while (total <= 2_459_000) {
+            total = total + salary;
+            month++;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
+        }
     }
 }
