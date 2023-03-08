feab16bd38379e05e3a15fc9793178b172f4afe5
Linke <linke@duck.com>
2023-03-08 06:12:29 +0300

    task 8 complete task 7 fix

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
     }
+
     public static void task1() {
         int salary = 15000;
         int total = 0;
@@ -18,6 +20,7 @@ public class Main {
             System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
         }
     }
+
     public static void task2() {
         int i = 0;
         while (i < 10) {
@@ -25,11 +28,12 @@ public class Main {
             System.out.print(i + " ");
         }
         System.out.println();
-        for(i = 10; i > 0; i--) {
+        for (i = 10; i > 0; i--) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
+
     public static void task3() {
         int people = 12_000_000;
         int death = 8 * 12000;
@@ -41,6 +45,7 @@ public class Main {
             System.out.println("Год " + year + " численность населения составляет " + people);
         }
     }
+
     public static void task4() {
         int salary = 15000;
         int total = 0;
@@ -52,6 +57,7 @@ public class Main {
             System.out.println("Месяц " + month + " сумма накоплений равна " + total);
         }
     }
+
     public static void task5() {
         int salary = 15000;
         int total = 0;
@@ -65,6 +71,7 @@ public class Main {
             }
         }
     }
+
     public static void task6() {
         int salary = 15000;
         int total = 0;
@@ -75,22 +82,34 @@ public class Main {
             total = total + salary;
             month++;
             if (month % 6 == 0) {
-                    System.out.println("Год " + year + " Месяц " + month + " сумма накоплений равна " + total);
+                System.out.println("Год " + year + " Месяц " + month + " сумма накоплений равна " + total);
             }
             if (month % 12 == 0) {
                 year++;
             }
         }
     }
+
     public static void task7() {
-        int date = 7;
-        int day = 1;
-        while (day < 31) {
+        int date = 3;
+        int day = 0;
+        while (day < 5) {
             System.out.println("Сегодня пятница " + date + " число. Необходимо подготовить отчёт.");
             date = date + 7;
             day++;
-            if(date > 31) {
+            if (date > 31) {
                 break;
+            }
+        }
+    }
+
+    public static void task8() {
+        int currentYear = 2023;
+        int year200minus = currentYear - 200;
+        int year100plus = currentYear + 100;
+        for (int year = year200minus; year < year100plus; year++) {
+            if(year % 79 ==0) {
+                System.out.println(year);
             }
         }
     }
